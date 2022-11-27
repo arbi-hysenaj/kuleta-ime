d53ab5bfdf0b055a0e733e5afba2995d77f02a55
Arbi Hysenaj <hysenajarbi@gmail.com>
2022-11-27 18:43:48 +0100

    Connected the API and rendered the table

--- src/helpers/calculations.js
+++ src/helpers/calculations.js
@@ -6,6 +6,23 @@ document.addEventListener('DOMContentLoaded', () => {
         marketCapResult.textContent = (Math.round(data.data.total_market_cap.usd)).toLocaleString();
         marketVolumeResult.textContent = (Math.round(data.data.total_volume.usd)).toLocaleString();
     });
-
 })
+
+
+const tableContent= document.querySelector("#table-content");
+fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h")
+.then(response => response.json()).then(data => {
+    tableContent.innerHTML= "";
+    data.forEach(item=> {
+        tableContent.insertAdjacentHTML("beforeend", `<tr>
+             <th scope="row">${item.market_cap_rank}</th>
+             <td>${item.name}</td>
+             <td>${parseFloat(item.price_change_percentage_24h).toFixed(2)}</td>
+             <td>${item.current_price}$</td>
+             <td>${item.market_cap.toLocaleString()} $</td>
+             <td>${item.total_volume.toLocaleString()} $</td>
+             </tr>`)
+    })})
+
+
 
