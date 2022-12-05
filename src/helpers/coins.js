bd84eff9857e29ca1b4a80dc657daa72d29c4d29
Arbi Hysenaj <hysenajarbi@gmail.com>
2022-12-05 16:40:08 +0100

    .

---
+++ src/helpers/coins.js
@@ -0,0 +1,146 @@
+export const coins = [
+    {
+    "coin": "Bitcoin",
+    },
+    {
+    "coin": "Ethereum",
+    },
+    {
+    "coin": "Tether",
+    },
+    {
+    "coin": "BNB",
+    },
+    {
+    "coin": "USD Coin",
+    },
+    {
+    "coin": "BUSD",
+    },
+    {
+    "coin": "XRP",
+    },
+    {
+    "coin": "Dogecoin",
+    },
+    {
+    "coin": "Cardano",
+    },
+    {
+    "coin": "Polygon",
+    },
+    {
+    "coin": "Polkadot",
+    },
+    {
+    "coin": "Litecoin",
+    },
+    {
+    "coin": "Shiba Inu",
+    },
+    {
+    "coin": "OKB",
+    },
+    {
+    "coin": "Dai",
+    },
+    {
+    "coin": "Solana",
+    },
+    {
+    "coin": "Tron",
+    },
+    {
+    "coin": "Uniswap",
+    },
+    {
+    "coin": "Avalanche",
+    },
+    {
+    "coin": "Chainlink",
+    },
+    {
+    "coin": "Leo Token",
+    },
+    {
+    "coin": "Cosmos Hub",
+    },
+    {
+    "coin": "Ethereum Classic",
+    },
+    {
+    "coin": "Monero",
+    },
+    {
+    "coin": "Stellar",
+     },
+    {
+    "coin": "Bitcoin Cash",
+    },
+    {
+    "coin": "Quant",
+    },
+    {
+    "coin": "Cronos",
+    },
+    {
+    "coin": "Algorand",
+    },
+    {
+    "coin": "Filecoin",
+    },
+    {
+    "coin": "Apecoin",
+    },
+    {
+    "coin": "Near Protocol",
+    },
+    {
+    "coin": "Vechain",
+    },
+    {
+    "coin": "Internet Computer",
+    },
+    {
+    "coin": "Hedera",
+    },
+    {
+    "coin": "Flow",
+    },
+    {
+    "coin": "Terra Luna Classic",
+    },
+    {
+    "coin": "EOS",
+    },
+    {
+    "coin": "Frax",
+    },
+    {
+    "coin": "Trust Wallet",
+    },
+    {
+    "coin": "Sandbox",
+    },
+    {
+    "coin": "Tezos",
+    },
+    {
+    "coin": "Axie Infinity",
+    },
+    {
+    "coin": "Aave",
+    },
+    {
+    "coin": "Theta Network",
+    },
+    {
+    "coin": "Chiliz",
+    },
+    {
+    "coin": "Huobi",
+    },
+    {
+    "coin": "Decentraland",
+    }
+]
