a33558c29316c443344a19b006e385758f18a5ca
Arbi Hysenaj <hysenajarbi@gmail.com>
2022-12-06 19:09:21 +0100

    Added the balance and P/L section

--- src/helpers/addportfolio.js
+++ src/helpers/addportfolio.js
@@ -1,12 +1,18 @@
 let storage = window.localStorage;
-let portfolioStorage = storage.getItem('portfolios');
-console.log(storage, 'mystorage');
+let portfolioStorage = JSON.parse(storage.getItem('portfolios'));
+let transactionStorage = JSON.parse(storage.getItem('transactions'));
+import { calculatePnL, calculateTotalWorth } from "./calculations.js";
+import {getCoin} from "/src/helpers/fetches.js";
 
 const addPortofolioButton = document.querySelector(".btn-list");
 const popup = document.querySelector(".bg-popup");
 const addPortfolioForm = document.querySelector("#new-portfolio");
 const assetList = document.querySelector("#asset-list");
 const portfolioDropdown = document.querySelector("#portfolio-dropdown");
+
+document.addEventListener('transactionInserted', ()=> {
+  transactionStorage = JSON.parse(storage.getItem('transactions'));
+})
 
 addPortofolioButton.addEventListener("click", ()=> {
     popup.classList.add("show");
@@ -25,22 +31,44 @@ const updatePortfolioList = ({id , name}) => {
     portfolioDropdown.insertAdjacentHTML("beforeend", `<option value="${id}">${name}</option>`)
 }
 
-const updatePortfolios = (portfolio) => {
-    console.log(portfolioStorage, 'kitja')
+const insertPortfolios = (portfolio) => {
     if(!portfolioStorage){
       portfolioStorage = [portfolio]
     } else{ 
-      portfolioStorage = JSON.parse(portfolioStorage);
       portfolioStorage.push(portfolio);
-    }
-      storage.setItem('portfolios' , JSON.stringify(portfolioStorage));
+    } 
+    storage.setItem('portfolios' , JSON.stringify(portfolioStorage));
 }
 
 if(portfolioStorage){
-    JSON.parse(portfolioStorage).forEach(portfolio => {
+    portfolioStorage.forEach(portfolio => {
         updatePortfolioList(portfolio)
     })
 }
+
+const filterTransactions = (walletId) => {
+ return transactionStorage.filter(transaction => transaction.walletId === walletId.toLowerCase())
+
+}
+
+const updatePortfolioData = () => {
+const currentPortfolio = document.querySelector(".portfolio-list.active").innerHTML;
+const walletTransactions = filterTransactions(currentPortfolio)
+console.log(getCoin);
+let updatedTransaction = walletTransactions.map(item => {
+   item.currentPrice = getCoin(item.coin.toLowerCase()).current_price;
+   return item;
+  })
+console.log(calculateTotalWorth(updatedTransaction));
+let totalWorthElement = document.querySelector("#total-worth");
+let allPLElement = document.querySelector("#all-pl");
+let {total, totalInvested} = calculateTotalWorth(updatedTransaction)
+totalWorthElement.innerHTML= Math.round(total * 100)/ 100 + "$";
+allPLElement.innerHTML = Math.round(calculatePnL(total, totalInvested)*100) /100 + "%";
+
+}
+
+
 
 const checkList = () =>{
     const portfoliosList = document.querySelectorAll(".portfolios-list .portfolio-list");
@@ -48,36 +76,20 @@ const checkList = () =>{
         portfolioList.addEventListener("click", event => {
             document.querySelector(".portfolio-list.active").classList.remove("active");
             event.currentTarget.classList.add("active")
-
-            document.querySelector(".tracking-data.active").classList.remove("active")
-            const selector= event.currentTarget.dataset.content;
-            document.querySelector(selector).classList.add("active");
+            updatePortfolioData();
         })
     })
-}
+  }
+
 
 addPortfolioForm.addEventListener("submit", event => {
     event.preventDefault();
     const portfolioName = document.querySelector("#portfolio-name").value;
     const portfolio = {id: portfolioName.toLowerCase() , name: portfolioName}
     updatePortfolioList(portfolio);
-    // const dataContainer = document.querySelector(".data-container");
-    // dataContainer.insertAdjacentHTML("beforeend", 
-    // `<div class="tracking-data" id="${portfolioName.value.toLowerCase()}">
-    // <div class="tracking-container">
-    //     <div class="current-net-worth">
-    //         10$
-    //     </div>
-    //     <div class="add-transaction">
-    //     <a href="">Add Transaction</a> 
-    //     </div>
-    // </div>
-    // <div class="all-time-pl">
-    //     +10%
-    // </div>
-    // </div>`)
-    updatePortfolios(portfolio);
+    insertPortfolios(portfolio);
     checkList();
     document.querySelector(".bg-popup").classList.remove("show");    
 })
 
+checkList();
---
+++ src/helpers/calculations.js
@@ -0,0 +1,13 @@
+export const calculateTotalWorth = (transactions) =>{
+    let total = 0, totalInvested = 0;
+    transactions.forEach(element => { 
+        total += (element.currentPrice * element.amount)
+        totalInvested += (element.purchasePrice * element.amount)
+    });
+    return {total, totalInvested};
+}
+
+export const calculatePnL = (total, totalInvested) =>{
+    return ((total-totalInvested)/ totalInvested ) *100; 
+}
+
